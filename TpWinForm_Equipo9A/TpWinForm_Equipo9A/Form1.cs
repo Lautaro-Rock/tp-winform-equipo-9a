7f44f88b9d2668b114f948c935a5c0637f105f02
Lautaro Maldonado <lautylty@hotmail.com>
2025-04-12 01:28:55 -0300

    Creando home de inicio

--- TpWinForm_Equipo9A/TpWinForm_Equipo9A/Form1.cs
+++ TpWinForm_Equipo9A/TpWinForm_Equipo9A/Form1.cs
@@ -10,11 +10,42 @@ using System.Windows.Forms;
 
 namespace TpWinForm_Equipo9A
 {
-    public partial class Form1 : Form
+    public partial class titlePrincipal : Form
     {
-        public Form1()
+        public titlePrincipal()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 ventanaMenu = new Form2();
+            ventanaMenu.ShowDialog();
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
